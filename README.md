986b904aa58fadebca23833b537ee787dc4d35b9
Cazuist <sovrus@mail.ru>
2020-10-01 15:10:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+[![Code style: airbnb](https://img.shields.io/badge/code%20style-airbnb-blue.svg?style=flat-square)](https://github.com/airbnb/javascript)
+
+[![Build status](https://ci.appveyor.com/api/projects/status/sgy76wfxt2u360pt?svg=true)](https://ci.appveyor.com/project/Cazuist/ahj-8-animation)
+
+[GH-Pages](https://cazuist.github.io/ahj-8-animation)
+
+# Домашнее задание к лекции "Animation"
+### 1. Collapse
+### 2. Callback Chat
+### 3. Liker
---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import WidgetsManager from './WidgetsManager';
+import CollapseWidget from './CollapseWidget';
+import CallbackChatWidget from './CallbackChatWidget';
+import LikerWidget from './LikerWidget';
+
+const collapseWidget = new CollapseWidget(document.querySelector('.widget-collapse'));
+const callbackCahtWidget = new CallbackChatWidget(document.querySelector('.widget-callback'));
+const likerWidget = new LikerWidget(document.querySelector('.widget-liker'));
+
+const widgets = [collapseWidget, callbackCahtWidget, likerWidget];
+
+const manager = new WidgetsManager(widgets);
+manager.init();
